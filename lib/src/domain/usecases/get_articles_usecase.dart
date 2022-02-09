dd97bdd3007481e35360f83658cf4dbec4ae2a30
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-02-09 19:10:41 -0300

    add GetArticlesUseCase

---
+++ lib/src/domain/usecases/get_articles_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_clean_arch/src/core/params/article_request.dart';
+import 'package:flutter_clean_arch/src/core/resources/data_state.dart';
+import 'package:flutter_clean_arch/src/core/usecases/usecase.dart';
+import 'package:flutter_clean_arch/src/domain/entities/article.dart';
+import 'package:flutter_clean_arch/src/domain/repositories/articles_repository.dart';
+
+class GetArticlesUseCase
+    implements UseCase<DataState<List<Article>>, ArticlesRequestsParams> {
+  GetArticlesUseCase(this._articlesRepository);
+
+  final ArticlesRepository _articlesRepository;
+
+  @override
+  Future<DataState<List<Article>>> call({
+    required ArticlesRequestsParams params,
+  }) {
+    return _articlesRepository.getBreakingNewsArticles(params);
+  }
+}
