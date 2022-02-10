7a0311efc3880385b18a2692893d289834caf993
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-02-10 07:30:44 -0300

    add models

---
+++ lib/src/domain/repositories/articles_repository.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_clean_arch/src/core/params/article_request.dart';
+import 'package:flutter_clean_arch/src/core/resources/data_state.dart';
+import 'package:flutter_clean_arch/src/domain/entities/article.dart';
+
+abstract class ArticlesRepository {
+  Future<DataState<List<Article>>> getBreakingNewsArticles(
+    ArticlesRequestsParams params,
+  );
+}
